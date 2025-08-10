2f4c286ca6a71b4e378513bf7f3be8cb9a38a19f
kentaro-s <kentarosaiga@gmail.com>
2025-08-10 23:03:54 +0900

    github-actionsの処理を追加

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def sum(a: int, b: int) -> int:
 def main():
     greet()
     result = sum(5, 3)
-    print(f"The sum of 5 and 3 is: {result}")   
+    print(f"THe answer is: {result}")   
 
 if __name__ == "__main__":
     main()
